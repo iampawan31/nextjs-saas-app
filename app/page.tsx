4753df9adde3519f00196b3495af3777da7673ce
Pawan Kumar <surpawan@gmail.com>
2025-07-14 14:59:11 +0530

    add: add homepage UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,50 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my Saas app</h1>
-      <Button>Let&apos;s get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="aaa"
+          name="Neura the Brainy explorer"
+          topic="Neural network of the brain"
+          duration={45}
+          subject="science"
+          color="#ff0000"
+        />
+        <CompanionCard
+          id="bbb"
+          name="Countsy the number wizard"
+          topic="Derivates and Integrals"
+          duration={30}
+          subject="maths"
+          color="#00ff00"
+        />
+        <CompanionCard
+          id="ccc"
+          name="Verba the vocabulary builder"
+          topic="English literature"
+          duration={30}
+          subject="english literature"
+          color="#0000ff"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently Completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
