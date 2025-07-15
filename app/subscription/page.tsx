6c50cd25db16860a40107903f41e33002847a518
Pawan Kumar <surpawan@gmail.com>
2025-07-15 20:37:03 +0530

    add: subscription UI and Logic using Clerk Billing

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,11 @@
+import { PricingTable } from '@clerk/nextjs'
+
 const Subscription = () => {
-  return <div>Subscription</div>
+  return (
+    <div className="p-4 mt-10 min-lg:max-w-7xl flex justify-center items-center mx-auto">
+      <PricingTable />
+    </div>
+  )
 }
 
 export default Subscription
